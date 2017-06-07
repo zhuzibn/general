38d17bd2f216a9950bbed016fa2e886346000bba
Brian <Brian@ECE-1PN5LG2>
2017-06-07 23:59:06 +0800

    update constantfile.m

--- constant/constantfile.m
+++ constant/constantfile.m
@@ -1,6 +1,6 @@
-gam=175881998755.578;%rad/(s.T)
+gam=1.760859644e11;%rad/(s.T)
 ele=1.60200000000000e-19;%[C]
 mu_0=1.25663706143592e-06;%T.m/A
 mub=9.274e-24;%[J/T]bohr magneton
-hbar=6.58211971233181e-16;%eV.s
-kb=1.38e-23;%[J/K]boltzman constant
+hbar=6.58211951440e-16;%eV.s
+kb=1.38064852e-23;%[J/K]boltzman constant

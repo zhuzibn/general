7e0067b46c1ee9f2fead93a446866ebef4b5f4d2
zhuzibn <zzfmvp@gmail.com>
2017-06-07 17:26:14 +0800

    Create constantfile.m

---
+++ constant/constantfile.m
@@ -0,0 +1,6 @@
+g=175881998755.578;%rad/(s.T)
+e=1.60200000000000e-19;%[C]
+mu_0=1.25663706143592e-06;%T.m/A
+mub=9.274e-24;%[J/T]bohr magneton
+hbar=6.58211971233181e-16;%eV.s
+kb=1.38e-23;%[J/K]boltzman constant

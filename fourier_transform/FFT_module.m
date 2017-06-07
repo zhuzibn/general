16c3f1eca9c618a764519196f6895aec29149627
a0132576 <a0132576@7RXR622.stf.nus.edu.sg>
2017-06-07 13:38:18 +0800

    update crip and plot setting

--- fourier_transform/FFT_module.m
+++ fourier_transform/FFT_module.m
@@ -3,53 +3,47 @@
 % 1. nT: number of sampling points
 % 2. runTime: time duration, [ns]
 % 3. y: time domain information
-function centfreq=FFT_module(nT0,runTime0,y)
+% 4. rminit:remove percentage of data from the beginning
+% 5. rmlast:remove percentage of data from the beginning
+% 6. plotfft, plot FFT spectrum (1) or not(0)
+function centfreq=FFT_module(nT0,runTime0,y,rminit,rmlast,plotfft)
 nT=nT0;
 runTime=runTime0;
-plottotal=0; %plot total spectrum or partial
+%plottotal=0; %plot total spectrum or partial
 
-rm_ini_t=1; %1: remove first part data; 0: keep all data.
-if (rm_ini_t)
-    rm_amount_head=0.3; %remove first 1/8 data
-    nT=ceil(nT-nT0*rm_amount_head);
-    runTime=runTime-runTime0*rm_amount_head;
-    y=y(:,(end-nT):end);
-end
+rm_amount_head=rminit; %remove first xx data
+nT=ceil(nT-nT0*rm_amount_head);
+runTime=runTime-runTime0*rm_amount_head;
+y=y(:,(end-nT):end);
 
-rm_final_t=0; %1: remove last quarter data; 0: keep all data.
-if (rm_final_t)
-    rm_amount_tail=1/8;%remove last 1/8 data
-    nT=ceil(nT-nT0*rm_amount_tail);
-    runTime=runTime-runTime0*rm_amount_tail;
-    y=y(:,1:nT);
-end
+rm_amount_tail=rmlast;%remove last 1/8 data
+nT=ceil(nT-nT0*rm_amount_tail);
+runTime=runTime-runTime0*rm_amount_tail;
+y=y(:,1:nT);
 
 L=nT;
 Fs=L/(runTime/1e9); %sampling frequency
 %% total frequency domain
 freq_1=(0:ceil(L/2))*Fs/L;     % frequency vector
 
-if (1) %% FFT for y
-    tmp1=2*abs(fft(y)/L);%fourier transform of mx, double side spectrum
-    fftx_1=tmp1(5:ceil(L/2)+1); % single side spectrum
-    switch plottotal
-        case 1
-            figure;
-            plot(freq_1(5:end)*1e-9,fftx_1)
-            xlabel('frequency(GHz)');ylabel('mV');
-        case 0
-            [~,col]=max(fftx_1); % locate fundamental frequency
-            centfreq=freq_1(col)*1e-9;
-            
-            figure('name','FFT-mx')
-            plot(freq_1(ceil(col/2):col*4)*1e-9,fftx_1(ceil(col/2):col*4))
-            xlabel('frequency(GHz)');ylabel('mV');
-            
-    end
-    
+tmp1=2*abs(fft(y)/L);%fourier transform of mx, double side spectrum
+fftx_1=tmp1(5:ceil(L/2)+1); % single side spectrum
+
+if plotfft
+    figure;
+    plot(freq_1(5:end)*1e-9,fftx_1)
+    xlabel('frequency(GHz)');ylabel('mV');
 end
 
-if (0) %PSD for y
+[~,col]=max(fftx_1); % locate fundamental frequency
+centfreq=freq_1(col)*1e-9;
+if (0)%debug use, visualization
+figure('name','FFT-mx')
+plot(freq_1(ceil(col/2):col*4)*1e-9,fftx_1(ceil(col/2):col*4))
+xlabel('frequency(GHz)');ylabel('mV');
+end
+
+if (0) %to do, PSD for y
     tmp1=fft(y);
     tmp1=tmp1(1:ceil(L/2)+1);
     psdd=(1/(L^2))*abs(tmp1).^2;
